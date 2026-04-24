0c2c84e240c2cd6c3c7ff2450ceff096ff302bfb
risansan30 <fantianlizi3@gmail.com>
2026-04-24 19:59:33 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const modalTrigger = document.querySelector('.trigger')
 const modal = document.querySelector('.modal')
 const modalClose = document.querySelector('.close')
+modalTrigger.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
 modalClose.addEventListener('click', () => {
     modal.style.display = 'none';
 })
